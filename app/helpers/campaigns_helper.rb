0a2370d783d400039adac8ca771dac5fed53eb5a
Simao Belchior <simaobelchior@gmail.com>
2016-10-24 14:41:23 +0100

    Load correct image if present

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,2 +1,15 @@
 module CampaignsHelper
+  def article_with_background campaign, &block
+    bkg_image = if campaign.background_image
+                 "url('#{campaign.background_image(:original)}')"
+                else
+                  nil
+                end
+
+    content_tag(:article,
+                class: "l-intro #{"-campaigns-detail" unless bkg_image}",
+                style: "#{"background-image: #{bkg_image}" if bkg_image}") do
+      yield
+    end
+  end
 end
