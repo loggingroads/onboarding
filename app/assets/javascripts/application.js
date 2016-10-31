289e62e242b40005701484a0f3e72702863602a8
edbrett <e.j.a.brett@gmail.com>
2016-10-31 19:04:02 +0100

    add js smooth scroll for homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,6 +50,22 @@ $(document).on('page:load ready', function () {
     $.get($('#search-form-tasks').attr('action'), $('#search-form-tasks').serialize(), null, 'script');
     return false;
   });
+
+  $(function() {
+     $('a[href*="#"]:not([href="#"])').click(function() {
+       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+         var target = $(this.hash);
+         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+           $('html, body').animate({
+             scrollTop: target.offset().top
+           }, 500);
+           return false;
+         }
+       }
+     });
+   });
+   
 });
 
 $(document).ready(function(){
