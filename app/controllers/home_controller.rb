499c20b7599252349917a7679d40576f7852964d
Simao Belchior <simaobelchior@gmail.com>
2016-12-06 17:37:00 +0000

    Slide through events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.where(end_date: nil).or(Event.where("end_date < ?", Date.today))
+    @events = Event.where(end_date: nil).or(Event.where("end_date >= ?", Date.today))
     @events = @events.order("date DESC")
     @campaigns = Campaign.where(status: CampaignStatus::LIVE).order(:start_date)
   end
