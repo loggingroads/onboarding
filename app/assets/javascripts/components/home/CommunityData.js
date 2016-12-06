e6d7e7f0a680718a08c532c0d7060b15ab4c661f
j8seangel <jseangel@gmail.com>
2016-12-06 20:20:20 +0100

    Fix new api endpoint data structure

--- app/assets/javascripts/components/home/CommunityData.js
+++ app/assets/javascripts/components/home/CommunityData.js
@@ -5,28 +5,26 @@ import React from 'react';
 class CommunityData extends React.Component {
 
   componentWillMount() {
-    this.props.setCommunityData(this.props.communityData);
+    this.props.setCommunityData();
   }
 
   render() {
     const communityData = this.props.communityData;
- 
-    console.log(communityData);
 
-    if (communityData && !Object.keys(communityData).length) return <div className="community-data" />;
+    if (!communityData || !Object.keys(communityData).length) return <div className="community-data" />;
     return (
       <div className="community-data">
         <div className="community-element" key="users">
           <h3 className="text text-legend -primary">users</h3>
-          <p className="text text-numeric-m -darker">{communityData && communityData.users.length}</p>
+          <p className="text text-numeric-m -darker">{communityData && Object.keys(communityData.users).length}</p>
         </div>
         <div className="community-element" key="edits">
           <h3 className="text text-legend -primary">edits</h3>
-          <p className="text text-numeric-m -darker">{communityData && communityData.length}</p>
+          <p className="text text-numeric-m -darker">{communityData && Object.keys(communityData.times).length}</p>
         </div>
         <div className="community-element" key="roads">
           <h3 className="text text-legend -primary">roads</h3>
-          <p className="text text-numeric-m -darker">{communityData && communityData.times.roads}</p>
+          <p className="text text-numeric-m -darker">{communityData && communityData.total.roads}</p>
         </div>
       </div>
     );
