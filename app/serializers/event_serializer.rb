7aff14c13b9454bc25031992d2d789135a5f82ba
Simao Belchior <simaobelchior@gmail.com>
2017-03-10 12:40:04 +0000

    Include htag in the serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,6 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image, :url, :date, :location, :instructions, :contact, :campaign_id
+  attributes :id, :name, :description, :image, :url, :date, :location,
+    :instructions, :contact, :campaign_id, :htag
 
   belongs_to :campaign
   has_many :tasks
--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,7 +1,11 @@
 class TaskSerializer < ActiveModel::Serializer
   attributes :id, :name, :task_manager_url, :task_type, :description,
-    :image, :status, :location, :created_at
+    :image, :status, :location, :created_at, :htag
 
   belongs_to :campaigns
   belongs_to :events
+
+  def image
+    object.image.url
+  end
 end
