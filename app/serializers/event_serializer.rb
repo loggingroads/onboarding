307d82ff93df732e58655b9b05e997db30b101b2
Simao Belchior <simaobelchior@gmail.com>
2017-03-10 13:25:01 +0000

    Some more bits of data

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,11 +1,15 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :image, :url, :date, :location,
-    :instructions, :contact, :campaign_id, :htag
+    :instructions, :contact, :campaign_id, :htag, :location_name, :task_count
 
   belongs_to :campaign
   has_many :tasks
 
   def image
     object.image.url
+  end
+
+  def task_count
+    object.tasks.count
   end
 end
